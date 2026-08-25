775161c87716b291866d8557f4f1a469dec27275
nycollassobolevski <sobolevski.academy@gmail.com>
2026-08-25 16:04:13 -0300

    feat slider component ui ready

---
+++ ui/Next/Shared/Components/Modules/SoundSlider/SoundSlider.axaml.cs
@@ -0,0 +1,25 @@
+
+
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
+
+namespace Next.Shared.Components.Modules;
+
+public partial class SoundSlider : Module
+{
+
+    public int CurrentValue { get; set; } = 75;
+
+    public SoundSlider() : base("Slider Module 01",0)
+    {
+        InitializeComponent();
+        ModuleLabel.Text = this.ModuleName;
+        SliderValue.Value = CurrentValue;
+    }
+
+    public void OnChange(object sender, RangeBaseValueChangedEventArgs args)
+    { 
+        CurrentValue = (int)args.NewValue;
+        currnentValueText.Text = CurrentValue.ToString();
+    }
+}
